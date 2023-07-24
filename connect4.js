45f5c0deb01546e724edcd4b944b58ed8b6d6063
Hao Lin <hao.lin0728@gmail.com>
2023-07-23 23:34:30 -0400

    step 2 and 3 done

--- connect4.js
+++ connect4.js
@@ -4,30 +4,25 @@
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
  * board fills (tie)
  */
-const myGameInstances = []; //manage game instances here
-const currentGameInstance = myGameInstances[0] || null; 
-let currentPlayerInCurrentInstance = isThereAGameChecker() ? currentGameInstance.currPlayer : null;
-let playerOne = isThereAGameChecker() ? currentGameInstance.playersArr[currentGameInstance.playersArr.indexOf('PlayerOne')] : null; //assign this to instance of player 1 running currently
-let playerTwo = isThereAGameChecker() ? currentGameInstance.playersArr[currentGameInstance.playersArr.indexOf('PlayerTwo')] : null; //assign this to instance of player 2 running currently
 
 class ConnectFourGame{
   constructor (WIDTH, HEIGHT){
     this.WIDTH = WIDTH;
     this.HEIGHT = HEIGHT;
-    this.currPlayer = []; // active player: 1 or 2 
-    this.playersArr = [] //add new players here 
+    this.playersArr = [null,null] //add new players here ; null value if no players are added
+    this.currPlayer = this.playersArr[0]; // default: currPlayer: null; active player: 1 or 2 
     this.board = this.makeBoard() // array of rows, each row is array of cells  (board[y][x])
     this.makeHtmlBoard();
     this.gameOver = false;
     this.element = document.querySelector('table#board')
-    myGameInstances.push(this)
+    // myGameInstances.push(this)
   } 
   /** makeBoard: create in-JS board structure:
    *   board = array of rows, each row is array of cells  (board[y][x])
-   */
-  
-  makeBoard = () => {
-    let blankBoard = [];
+  */
+ 
+ makeBoard = () => {
+   let blankBoard = [];
     for (let y = 0; y < this.HEIGHT; y++) {
       blankBoard.push(Array.from({ length: this.WIDTH }));
     }
@@ -44,15 +39,15 @@ class ConnectFourGame{
     top.addEventListener('click',evt => {
       this.handleClick(evt)
     })
-  
+    
     for (let x = 0; x < this.WIDTH; x++) {
       const headCell = document.createElement('td');
       headCell.setAttribute('id', x);
       top.append(headCell);
     }
-  
+    
     board.append(top);
-  
+    
     // make main part of board
     for (let y = 0; y < this.HEIGHT; y++) {
       const row = document.createElement('tr');
@@ -62,17 +57,17 @@ class ConnectFourGame{
         cell.setAttribute('id', `${y}-${x}`);
         row.append(cell);
       }
-  
+      
       board.append(row);
     }
   }
-
-/** findSpotForCol: given column x, return top empty y (null if filled) */
-
-findSpotForCol = x => {
-  for (let y = this.HEIGHT - 1; y >= 0; y--) {
-    if (!this.board[y][x]) {
-      return y;
+  
+  /** findSpotForCol: given column x, return top empty y (null if filled) */
+  
+  findSpotForCol = x => {
+    for (let y = this.HEIGHT - 1; y >= 0; y--) {
+      if (!this.board[y][x]) {
+        return y;
     }
   }
   return null;
@@ -83,9 +78,12 @@ findSpotForCol = x => {
 placeInTable = (y, x) => {
   const piece = document.createElement('div');
   piece.classList.add('piece');
-  piece.classList.add(`p${this.currPlayer.playerName}`); //add name of player who added piece
+  piece.classList.add(`p${this.currPlayer.playerOrder}`); //add name of player who added piece
   piece.style.top = -50 * (y + 2);
-
+  if (this.currPlayer.playerColor !== 'red' || this.currPlayer.playerColor !== 'blue'){
+    piece.style.backgroundColor = this.currPlayer.playerColor  
+  }
+  
   const spot = document.getElementById(`${y}-${x}`);
   spot.append(piece);
 }
@@ -103,28 +101,29 @@ handleClick = evt => { //standard function -> this is now the html element that 
   // get x from ID of clicked cell
   const x = +evt.target.id;
   // switch players
-  this.currPlayer = this.currPlayer === playersArr[0] ? playersArr[1] : playersArr[0];
+  arrayToggler(this.playersArr)
+  this.currPlayer = this.playersArr[0] 
 
   // get next spot in column (if none, ignore click)
   const y = this.findSpotForCol(x);
   if (y === null) {
     return;
   }
-
+  
   // place piece in board and add to HTML table
-  this.board[y][x] = this.currPlayer;
+  this.board[y][x] = this.currPlayer.playerOrder;
   this.gameOver ? alert ('Game is over!'): this.placeInTable(y, x);
   
   // check for win
   if (this.checkForWin()) {
-    return this.endGame(`Player ${this.currPlayer} won!`);
+    return this.endGame(`Player ${this.currPlayer.playerName} won!`);
   }
   
   // check for tie
   if (this.board.every(row => row.every(cell => cell))) {
     return this.endGame('Tie!');
   }
-    
+  
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -133,22 +132,22 @@ checkForWin = () => {
   const _win = cells => {
     return cells.every(
       ([y, x]) =>
-        y >= 0 &&
-        y < this.HEIGHT &&
-        x >= 0 &&
-        x < this.WIDTH &&
-        this.board[y][x] === this.currPlayer
-    );
-  }
-  for (let y = 0; y < this.HEIGHT; y++) {
-    for (let x = 0; x < this.WIDTH; x++) {
-      // get "check list" of 4 cells (starting here) for each of the different
-      // ways to win
+      y >= 0 &&
+      y < this.HEIGHT &&
+      x >= 0 &&
+      x < this.WIDTH &&
+      this.board[y][x] === this.currPlayer.playerOrder
+      );
+    }
+    for (let y = 0; y < this.HEIGHT; y++) {
+      for (let x = 0; x < this.WIDTH; x++) {
+        // get "check list" of 4 cells (starting here) for each of the different
+        // ways to win
       const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
       const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
       const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
       const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
+      
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
@@ -159,28 +158,39 @@ checkForWin = () => {
 }//end of ConnectFourGame class
 
 //Part 2: Small Improvements - start/restart game button, no more clicking once game done
+let myGameInstances = [null]; //manage game instances here, nested array tracks players
+let currentGameInstance = myGameInstances[0] //default is null
 const startGameButton = document.getElementById('game-start-button')
 startGameButton.addEventListener('click', (e) => {
-  isThereAGameChecker() ? resetGame() : new ConnectFourGame(7,6);
+  e.preventDefault();
+  isThereAGameChecker() ? resetGame() : startGame();
+  getFormValuesAndMakeNewPlayers() /*grab input values and make new player classes*/
 })
+
+const startGame = (WIDTH = 7, HEIGHT = 6) =>{
+  myGameInstances[0] = new ConnectFourGame(WIDTH,HEIGHT)
+  return currentGameInstance = myGameInstances[0];
+}
 
 const resetGame = () => {
   const pieceDOMList = document.querySelectorAll('td > div.piece')
   pieceDOMList.forEach(piece => piece.remove())
+  currentGameInstance.gameOver = false;
   currentGameInstance.board = currentGameInstance.makeBoard();
 }
 
 const isThereAGameChecker = () => {
-  return currentGameInstance ? true : false; 
+  return (myGameInstances[0] !== null) ? true : false; 
 }
 
+let currentPlayerInCurrentInstance = isThereAGameChecker() ? currentGameInstance.currPlayer : null;
 
 //Part 3: Player class & color input
 class Player {
   constructor (playerName, playerColor){
     this.playerName = playerName;
     this.playerColor = playerColor;
-    playersArr.push(this)
+    // currentGameInstance.playersArr.push(this)
   }
   get nameAndColor () {
     return `${this.playerName}: ${this.playerColor}`
@@ -189,20 +199,20 @@ class Player {
 
 class PlayerOne extends Player {
   constructor(playerName,playerColor){
-    this.playerOrder = 1;
     super(playerName,playerColor)
+    this.playerOrder = 1;
   }
   set playerOneNameSetter(playerName) {
-    if (typeof playerName === 'string'){
-      this.playerName = playerName;
+    if (playerName === '' || typeof playerName !== 'string' || playerName !== 1){
+      this.playerName = 1;
     }
     else {
-      this.playerName = '1'
+      this.playerName = playerName
       return 
     }
   }
   set playerOneColorSetter(playerColor){
-    if (typeof this.playerColor === 'string'){
+    if (this.playerColor === ''){
       this.playerColor = playerColor;
     }
     else {
@@ -213,42 +223,52 @@ class PlayerOne extends Player {
 
 class PlayerTwo extends Player {
   constructor(playerName,playerColor){
-    this.playerOrder = 2;
     super(playerName,playerColor)
+    this.playerOrder = 2;
   }
   set playerTwoNameSetter(playerName) {
-    if (typeof playerName === 'string'){
-      this.playerName = playerName;
+    if (playerName === ''){
+      this.playerName = 2
     }
     else {
-      this.playerName = '2'
+      this.playerName = playerName;
       return 
     }
   }
   set playerTwoColorSetter(playerColor){
-    if (typeof this.playerColor === 'string'){
-      this.playerColor = playerColor;
+    if (this.playerColor === ''){
+      this.playerColor = 'blue'
     }
     else {
-      this.playerColor = 'blue'
+      this.playerColor = playerColor;
     }
   }
 }
 
 const arrayToggler = array => {
   [array[0],array[1]] = [array[1], array[0]]
+  return array[0]
 }
 
-const getFormValuesAndMakeNewPlayers = () => {
+/*TESTING PURPOSES DELETE AFTER*/
+let playerOne = isThereAGameChecker() ? currentGameInstance.playersArr[currentGameInstance.playersArr.indexOf('PlayerOne')] : null; //assign this to instance of player 1 running currently
+let playerTwo = isThereAGameChecker() ? currentGameInstance.playersArr[currentGameInstance.playersArr.indexOf('PlayerTwo')] : null; //assign this to instance of player 2 running currently
+console.log(myGameInstances,playerOne, playerTwo)
+
+/*grab input values and make new player classes*/
+getFormValuesAndMakeNewPlayers = () => {
   let p1Name = document.querySelector('input#playerOneName').value;
   let p1Color = document.querySelector('input#playerOneColor').value;
   
   let p2Name = document.querySelector('input#playerTwoName').value
   let p2Color = document.querySelector('input#playerTwoColor').value
 
-  const newplayerOne = new PlayerOne(p1Name, p1Name)
+  const newplayerOne = new PlayerOne(p1Name, p1Color)
   const newPlayerTwo = new PlayerTwo(p2Name, p2Color)
-
-  currentGameInstance.currPlayer.push(newplayerOne, newPlayerTwo) 
-  return 
+  
+  currentGameInstance.playersArr = [newplayerOne, newPlayerTwo]
+  currentGameInstance.currPlayer = newplayerOne
+  
+  currentGameInstance.playersArr.forEach(playerClass => console.log(playerClass.playerColor))
+  return
 }
